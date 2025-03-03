b1ad5f9b78a309ecb8304b1f5e396d326b288e50
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2025-03-03 06:43:23 +0100

    release(1.0.3): update name
    updated the name to Quran Notes Keeper

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "quran-notes-keeper-v1";
-const APP_VERSION = "1.0.2"; // Change this value whenever you make updates
+const APP_VERSION = "1.0.3"; // Change this value whenever you make updates
 
 // Install the service worker and cache assets
 self.addEventListener("install", (event) => {
